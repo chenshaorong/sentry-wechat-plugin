f4a4445421810d4fee3a763af0265d5ea93be140
chenshaorong <731685435@qq.com>
2019-05-25 08:44:26 +0800

    v1.01
    Signed-off-by: chenshaorong <731685435@qq.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="sentry-wechat-plugin",
-    version='1.0',
+    version='1.01',
     author='shaorong.chen',
     author_email='shaorong.chen@aliyun.com',
     url='https://github.com/chenshaorong/sentry-wechat-plugin',
